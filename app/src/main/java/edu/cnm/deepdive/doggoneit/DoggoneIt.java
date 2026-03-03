5b69e4541c8dd792f787572d6552ee3eac363df4
Kevin Dilts <kdilts@cnm.edu>
2026-03-03 16:51:41 -0700

    Commit by Kevin (Junie missed some files):Align package declaration with project name (edu.cnm.deepdive.doggoneit).

--- app/src/main/java/edu/cnm/deepdive/doggoneit/DoggoneIt.java
+++ app/src/main/java/edu/cnm/deepdive/doggoneit/DoggoneIt.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package edu.cnm.deepdive.myproject;
+package edu.cnm.deepdive.doggoneit;
 
 import android.app.Application;
 import dagger.hilt.android.HiltAndroidApp;
