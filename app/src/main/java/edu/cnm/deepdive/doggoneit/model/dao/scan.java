bad293ba3f05f5763b0ff110c57af1562c766091
Kevin Dilts <kdilts@cnm.edu>
2026-03-13 12:03:25 -0600

    create blank classes and interfaces for DAO and entity interfaces for milestone 2

---
+++ app/src/main/java/edu/cnm/deepdive/doggoneit/model/dao/scan.java
@@ -0,0 +1,5 @@
+package edu.cnm.deepdive.doggoneit.model.dao;
+
+public interface scan {
+
+}
---
+++ app/src/main/java/edu/cnm/deepdive/doggoneit/service/Repository.java
@@ -0,0 +1,7 @@
+package edu.cnm.deepdive.doggoneit.service;
+
+import androidx.room.RoomDatabase;
+
+public abstract class Repository extends RoomDatabase {
+
+}
