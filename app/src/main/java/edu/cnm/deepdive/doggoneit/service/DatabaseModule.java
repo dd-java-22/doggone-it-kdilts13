3d23a1da3a5f1c24894073921cafedceba40dab4
Kevin Dilts <kdilts@cnm.edu>
2026-03-16 11:17:16 -0600

    Commit by Codex: Add Hilt database module
    Adds a Dagger/Hilt module that provides the Room database and DAO instances as singletons.

---
+++ app/src/main/java/edu/cnm/deepdive/doggoneit/service/DatabaseModule.java
@@ -0,0 +1,54 @@
+package edu.cnm.deepdive.doggoneit.service;
+
+import android.content.Context;
+import androidx.room.Room;
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.qualifiers.ApplicationContext;
+import dagger.hilt.components.SingletonComponent;
+import edu.cnm.deepdive.doggoneit.model.dao.BreedFactDao;
+import edu.cnm.deepdive.doggoneit.model.dao.BreedPredictionDao;
+import edu.cnm.deepdive.doggoneit.model.dao.ScanDao;
+import edu.cnm.deepdive.doggoneit.model.dao.UserProfileDao;
+import jakarta.inject.Singleton;
+
+@Module
+@InstallIn(SingletonComponent.class)
+public class DatabaseModule {
+
+  @Provides
+  @Singleton
+  DoggoneItDatabase provideDatabase(@ApplicationContext Context context) {
+    return Room.databaseBuilder(
+      context,
+      DoggoneItDatabase.class,
+      DoggoneItDatabase.DATABASE_NAME
+    ).build();
+  }
+
+  @Provides
+  @Singleton
+  UserProfileDao provideUserProfileDao(DoggoneItDatabase database) {
+    return database.getUserProfileDao();
+  }
+
+  @Provides
+  @Singleton
+  ScanDao provideScanDao(DoggoneItDatabase database) {
+    return database.getScanDao();
+  }
+
+  @Provides
+  @Singleton
+  BreedPredictionDao provideBreedPrediction(DoggoneItDatabase database) {
+    return database.getBreedPredictionDao();
+  }
+
+  @Provides
+  @Singleton
+  BreedFactDao provideBreedFact(DoggoneItDatabase database) {
+    return database.getBreedFactDao();
+  }
+
+}
