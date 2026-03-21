1199be1aa2ff91da79f7946dbd0b728db5e274ab
kdilts13 <kdilts13@gmail.com>
2026-03-21 17:48:50 -0600

    Change by Codex: Add main activity and nav shell
    Add MainActivity with bottom nav wiring and launcher entry.\nCreate nav graph, menu, and placeholder fragments with view binding.\nAdd XML layouts, strings, and icons for placeholder UI.
    
    Prompt: Now create the main activity and placeholder fragment structure for this app.

---
+++ app/src/main/java/edu/cnm/deepdive/doggoneit/MainActivity.java
@@ -0,0 +1,41 @@
+/*
+ *  Copyright 2026 CNM Ingenuity, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package edu.cnm.deepdive.doggoneit;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+import dagger.hilt.android.AndroidEntryPoint;
+import edu.cnm.deepdive.doggoneit.databinding.ActivityMainBinding;
+
+@AndroidEntryPoint
+public class MainActivity extends AppCompatActivity {
+
+  private ActivityMainBinding binding;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    binding = ActivityMainBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
+
+    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+    NavigationUI.setupWithNavController(binding.bottomNav, navController);
+  }
+
+}
---
+++ app/src/main/java/edu/cnm/deepdive/doggoneit/ui/HomeFragment.java
@@ -0,0 +1,62 @@
+/*
+ *  Copyright 2026 CNM Ingenuity, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package edu.cnm.deepdive.doggoneit.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+import dagger.hilt.android.AndroidEntryPoint;
+import edu.cnm.deepdive.doggoneit.R;
+import edu.cnm.deepdive.doggoneit.databinding.FragmentHomeBinding;
+
+@AndroidEntryPoint
+public class HomeFragment extends Fragment {
+
+  private FragmentHomeBinding binding;
+
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    binding = FragmentHomeBinding.inflate(inflater, container, false);
+    return binding.getRoot();
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    binding.takePhotoButton.setOnClickListener(
+        v -> NavHostFragment.findNavController(this)
+            .navigate(R.id.action_homeFragment_to_photoCaptureFragment));
+    binding.analyzeFromGalleryButton.setOnClickListener(
+        v -> NavHostFragment.findNavController(this)
+            .navigate(R.id.action_homeFragment_to_cameraGalleryFragment));
+    binding.viewSavedButton.setOnClickListener(
+        v -> NavHostFragment.findNavController(this)
+            .navigate(R.id.action_homeFragment_to_scansGalleryFragment));
+  }
+
+  @Override
+  public void onDestroyView() {
+    binding = null;
+    super.onDestroyView();
+  }
+
+}
